7ecfb8f497e9becacc5f28b2e8580ace53dfc73c
Wanda Hayes <whayeswsfg@gmail.com>
2020-09-29 14:05:59 -0400

    Initial commit

---
+++ src/main/java/com/whayes/API/whAPITestingDemo/WhApiTestingDemoApplication.java
@@ -0,0 +1,14 @@
+package com.whayes.API.whAPITestingDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhApiTestingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhApiTestingDemoApplication.class, args);
+		System.out.println("WH API Demo - local host started");
+	}
+
+}
